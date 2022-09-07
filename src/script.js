4b75991b53cad62f4979ce704e19c2103da70944
MohdSaifCedcoss <mohdsaif2022aug@cedcoss.com>
2022-09-07 12:34:57 +0530

    Added the comments and some style changes

--- src/script.js
+++ src/script.js
@@ -1,8 +1,12 @@
 let counter=0;
+
+// function to decrease the count by 1
 document.getElementById("decrement").onclick=function(){
     counter-=1;
     document.getElementById('count').innerHTML=counter;
 }
+
+//function to increase the count by 1
 document.getElementById("increment").onclick=function(){
     counter+=1;
     document.getElementById('count').innerHTML=counter;
